d9f828e00292525ca30cd3fffa345d3037115fc2
Unknown <creepsky@gmail.com>
2018-10-07 17:43:08 +0200

    Add different kind of exceptions

--- NexusParser/common/NexusException.cs
+++ NexusParser/common/NexusException.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
+using Nexus.gen;
 using Nexus.ir;
 
 namespace Nexus.common
@@ -29,8 +28,40 @@ namespace Nexus.common
 
     public class UnexpectedScopeException : PositionedException
     {
-        public UnexpectedScopeException(IPositioned element, Type got, IEnumerable<Type> expected)
-            : base(element, $"unexpected scope for {element.GetType().Name}, got {got.Name}, expected {string.Join(" or ", expected.Select(i => i.Name))}")
+        public UnexpectedScopeException(IPositioned element, string got, IEnumerable<string> expected)
+            : base(element, $"unexpected scope for {element.GetType().Name}, got {got}, expected {string.Join(" or ", expected)}")
+        { }
+    }
+
+    public class VariableNotFoundException : PositionedException
+    {
+        public VariableNotFoundException(IPositioned element, string name)
+            : base(element, $"could not find variable {name}")
+        { }
+    }
+
+    public class FunctionNotFoundException : PositionedException
+    {
+        public FunctionNotFoundException(IPositioned element, string name)
+            : base(element, $"could not find function {name}")
+        { }
+    }
+
+    public class TypeMismatchException : PositionedException
+    {
+        public TypeMismatchException(IPositioned element, string expected, string got)
+            : this(element, new []{expected}, got)
+        { }
+
+        public TypeMismatchException(IPositioned element, IEnumerable<string> expected, string got)
+             : base(element, $"expected {string.Join(" or ", expected)} but got {got}")
+        { }
+    }
+
+    public class RedeclarationException : PositionedException
+    {
+        public RedeclarationException(IPositioned overwritingElement, IPositioned oldElement, string name)
+            : base(overwritingElement, $"first declaration of {name} at line {oldElement.Line}, col {oldElement.Column}, redeclaration")
         { }
     }
 }
