d510b2507181245e283ddd1075b48fdd165fae3e
Unknown <creepsky@gmail.com>
2018-10-13 20:46:08 +0200

    Add FileParser class for parsing files

---
+++ NexusParser/FileParser.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Antlr4.Runtime;
+using Nexus.gen;
+
+namespace Nexus
+{
+    public static class FileParser
+    {
+        public static IList<ir.File> ParseDirectory(string path) =>
+            Directory.EnumerateFiles(path, "*.nx").Select(ParseFile).ToList();
+
+        public static ir.File ParseFile(string path)
+        {
+            var file = File.OpenRead(path);
+            var textReader = new StreamReader(file);
+            var input = new AntlrInputStream(textReader);
+            var lexer = new NexusLexer(input, Console.Out, Console.Error);
+            var tokenStream = new CommonTokenStream(lexer);
+            var parser = new NexusParser(tokenStream);
+            var ast = parser.file();
+            var visitor = new NexusGrammarVisitor();
+            return (ir.File) visitor.Visit(ast);
+        }
+        
+        public static void SaveCompilationUnit(string outputFolder, CompilationUnit compilationUnit)
+        {
+            var headerPath = Path.Combine(outputFolder, compilationUnit.Name + ".hpp");
+            var sourcePath = Path.Combine(outputFolder, compilationUnit.Name + ".cpp");
+            File.WriteAllText(headerPath, compilationUnit.Header);
+            File.WriteAllText(sourcePath, compilationUnit.Source);
+        }
+
+        public static void WriteFiles(string outputPath, IEnumerable<CompilationUnit> compilationUnits)
+        {
+            Directory.CreateDirectory(outputPath);
+            foreach (var unit in compilationUnits)
+                SaveCompilationUnit(outputPath, unit);
+        }
+    }
+}
--- NexusParser/Program.cs
+++ NexusParser/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using Antlr4.Runtime;
-using Nexus.gen;
+﻿#pragma warning disable CS3021
 
 namespace Nexus
 {
@@ -11,36 +6,10 @@ namespace Nexus
     {
         private static int Main(string[] args)
         {
-            var files = Directory.EnumerateFiles(args[0], "*.nx").Select(ParseFile).ToList();
-            var generator = new Generator(files);
-            generator.Generate();
-            generator.Check();
-            Directory.CreateDirectory(args[1]);
-            foreach (var compilationUnit in generator.Compile())
-                SaveCompilationUnit(args[1], compilationUnit);
+            var files = FileParser.ParseDirectory(args[0]);
+            var units = Compiler.Compile(files);
+            FileParser.WriteFiles(args[1], units);
             return 0;
-        }
-
-        private static ir.File ParseFile(string path)
-        {
-            var file = File.OpenRead(path);
-            var textReader = new StreamReader(file);
-            var input = new AntlrInputStream(textReader);
-            var lexer = new NexusLexer(input, Console.Out, Console.Error);
-            var tokenStream = new CommonTokenStream(lexer);
-            var parser = new NexusParser(tokenStream);
-            var ast = parser.file();
-            Debug.WriteLine(ast.ToStringTree(parser));
-            var visitor = new NexusGrammarVisitor();
-            return (ir.File) visitor.Visit(ast);
-        }
-
-        private static void SaveCompilationUnit(string outputFolder, CompilationUnit compilationUnit)
-        {
-            var headerPath = Path.Combine(outputFolder, compilationUnit.Name + ".hpp");
-            var sourcePath = Path.Combine(outputFolder, compilationUnit.Name + ".cpp");
-            File.WriteAllText(headerPath, compilationUnit.Header);
-            File.WriteAllText(sourcePath, compilationUnit.Source);
         }
     }
 }
