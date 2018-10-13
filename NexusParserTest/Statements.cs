ea11e58d4c0ba0ff77c09854f81a870c29b6307d
Unknown <creepsky@gmail.com>
2018-10-13 19:17:23 +0200

    Add first assignment unit test

---
+++ NexusParserTest/Statements.cs
@@ -0,0 +1,48 @@
+using Nexus.gen;
+using Nexus.ir.expr;
+using Xunit;
+using Nexus.ir.stmt;
+
+namespace NexusParserTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestAssignment()
+        {
+            var context = new Context();
+            
+            // int a = 0;
+            context.Add("a", new Variable
+            {
+                Name = "a",
+                Type = new SimpleType
+                {
+                    Name = "int"
+                },
+                Initialization = new I32
+                {
+                    Value = 0
+                }
+            });
+            
+            // a = 1;
+            var i = new AssignmentStatement
+            {
+                Left = new VariableLiteral
+                {
+                    Name = "a"
+                },
+                Right = new I32
+                {
+                    Value = 1
+                }
+            };
+            
+            i.Check(context);
+            i.Generate(context, GenerationPhase.ForwardDeclaration);
+            i.Generate(context, GenerationPhase.Declaration);
+            i.Generate(context, GenerationPhase.Definition);
+        }
+    }
+}
