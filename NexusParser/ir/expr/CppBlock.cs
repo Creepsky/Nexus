c8db85cf25d1b937c3798cf8336bd727bd40388a
Unknown <creepsky@gmail.com>
2018-10-14 22:51:34 +0200

    Add CppBlock class

---
+++ NexusParser/ir/expr/CppBlock.cs
@@ -0,0 +1,36 @@
+﻿using Nexus.gen;
+
+namespace Nexus.ir.expr
+{
+    public class CppBlock : Expression
+    {
+        public string Block { get; }
+        
+        public CppBlock(string block, int line, int column)
+        {
+            Block = block;
+            Line = line;
+            Column = column;
+        }
+
+        public override IGenerationElement Generate(Context context, GenerationPhase phase)
+        {
+            return this;
+        }
+
+        public override IType GetResultType(Context context)
+        {
+            return new SimpleType(TypesExtension.Void, 0);
+        }
+
+        public override void Check(Context context)
+        {
+            // TODO
+        }
+
+        public override void Print(PrintType type, Printer printer)
+        {
+            printer.Write(Block);
+        }
+    }
+}
