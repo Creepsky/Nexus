60ae53c2b390b9dc4bc215cd292508170c92102a
Unknown <creepsky@gmail.com>
2018-10-22 19:35:17 +0200

    Parse templates to IR

--- NexusParser/ir/expr/TemplateList.cs
+++ NexusParser/ir/expr/TemplateList.cs
@@ -3,7 +3,10 @@ using Nexus.gen;
 
 namespace Nexus.ir.expr
 {
-    public class TemplateList : Expression
+    public interface ITemplateList : IExpression
+    { }
+    
+    public class TemplateList : Expression, ITemplateList
     {
         public IList<string> Types { get; }
 
@@ -12,6 +15,29 @@ namespace Nexus.ir.expr
             Types = types;
         }
 
+        public override IGenerationElement Generate(Context context, GenerationPhase phase)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override IType GetResultType(Context context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Check(Context context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Print(PrintType type, Printer printer)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class VariadicTemplateList : Expression, ITemplateList
+    {
         public override IGenerationElement Generate(Context context, GenerationPhase phase)
         {
             throw new System.NotImplementedException();
