fd28874fab691ed168a3d16b7418b904ea893721
Creepsky <creepsky@gmail.com>
2018-10-27 18:12:26 +0200

    Add member access epxression

--- NexusParser/NexusGrammarVisitor.cs
+++ NexusParser/NexusGrammarVisitor.cs
@@ -205,6 +205,16 @@ namespace Nexus
             FilePath = FileParser.CurrentPath
         };
 
+        public override object VisitMember_access(NexusParser.Member_accessContext context)
+        {
+            return new MemberAccess(context.IDENTIFIER(0).GetText(), context.IDENTIFIER(1).GetText())
+            {
+                Line = context.Start.Line,
+                Column = context.Start.Column,
+                FilePath = FileParser.CurrentPath
+            };
+        }
+
         public override object VisitFunction_call(NexusParser.Function_callContext context) => new FunctionCall
         {
             Name = context.IDENTIFIER().GetText(),
